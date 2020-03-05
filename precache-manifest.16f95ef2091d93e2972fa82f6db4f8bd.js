a659a3cfc9efc57ce94847698d2283696531741b
Chris Oatley <chris@BREATHE-LT50.local>
2020-03-05 21:54:06 +0000

    Updates

---
+++ precache-manifest.16f95ef2091d93e2972fa82f6db4f8bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d7653d0651d2bed5ce4d22e875719d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5be3e66d8737fc0036c8",
+    "url": "/monsters-rolodex/static/css/main.849dfd33.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5be3e66d8737fc0036c8",
+    "url": "/monsters-rolodex/static/js/main.6cea5c52.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
